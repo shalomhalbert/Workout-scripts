386715ec163fcca8ffdfc629fecf872f83a485d7
Shalom Halbert <shalomhalbert@gmail.com>
2023-08-11 18:13:49 -0300

    Initial commit

---
+++ august_2023/main.py
@@ -0,0 +1,16 @@
+import requests
+
+url = "https://www.bodybuilding.com/exercises/finder?equipment=barbell%2Cbody-only%2Cdumbbell%2Cnone%2Cother"
+
+response = requests.get(url)
+print(response.status_code)
+print(response.text)
+
+# FIXME: From each exercise's details page, collect the following:
+#  Details page's url
+#  Exercise's name
+#  Workout type
+#  Main muscle worked
+#  Equipment used
+#  Difficulty level
+#  All images Instructions
