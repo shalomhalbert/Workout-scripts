c6fddf10f6cba14c012819a29f153561883c2040
Shalom Halbert <shalomhalbert@gmail.com>
2023-08-14 21:57:12 -0300

    Completed parsing logic

--- august_2023/aggregate_exercise_details.py
+++ august_2023/aggregate_exercise_details.py
@@ -5,19 +5,15 @@ from exercise import Exercise
 
 
 def get_exercise_details_urls() -> List[str]:
-    # exercise_details_urls = []
-    # with open('exercise_details_urls.txt') as exercise_details_urls_file:
-    #     for url in exercise_details_urls_file:
-    #         cleaned_url = url.strip()
-    #         exercise_details_urls.append(cleaned_url)
-    #
-    # return exercise_details_urls
+    exercise_details_urls = []
+    with open('exercise_details_urls.txt') as exercise_details_urls_file:
+        for url in exercise_details_urls_file:
+            cleaned_url = url.strip()
+            exercise_details_urls.append(cleaned_url)
 
-    # FIXME: Delete and uncomment code above.
-    return ["https://www.bodybuilding.com/exercises/palms-down-wrist-curl-over-a-bench"]
+    return exercise_details_urls
 
 
-# FIXME: Extract wanted data
 def parse_exercise(exercise_details_url: str) -> Exercise:
     exercise_details_request = requests.get(exercise_details_url)
     exercise_details_parser = BeautifulSoup(exercise_details_request.content, "html.parser")
@@ -25,9 +21,13 @@ def parse_exercise(exercise_details_url: str) -> Exercise:
     title_element = exercise_details_parser.find(name="h1")
     title = title_element.text.strip()
 
-    # FIXME: Replace href filter because it is too specific. Can a wildcard be used?
-    primary_muscle_element = exercise_details_parser.find(name="a", href="/exercises/muscle/forearms")
-    primary_muscle = primary_muscle_element.text.strip()
+    exercise_characteristics_list_element = exercise_details_parser.find(name="ul", class_="bb-list--plain")
+    exercise_characteristics_list_item_elements = exercise_characteristics_list_element.find_all(name="li")
+    primary_muscle = ""
+    for characteristic_list_item in exercise_characteristics_list_item_elements:
+        characteristic_list_item_text = characteristic_list_item.text.strip()
+        if "Main Muscle Worked:" in characteristic_list_item_text:
+            primary_muscle = characteristic_list_item.contents[1].text.strip()
 
     image_elements = exercise_details_parser.find_all(name="img", class_="ExImg ExDetail-img js-ex-enlarge")
     image_srcs = []
@@ -35,7 +35,16 @@ def parse_exercise(exercise_details_url: str) -> Exercise:
         src = image_element['src'].strip()
         image_srcs.append(src)
 
-    return Exercise(name=title, primary_muscle=primary_muscle, image_urls=image_srcs)
+    instructions_section_element = exercise_details_parser.find(name="section",
+                                                                class_="ExDetail-section ExDetail-guide")
+    instructions_list_element = instructions_section_element.find(name="ol")
+    instructions_elements = instructions_list_element.find_all("li")
+    instructions = []
+    for instruction_element in instructions_elements:
+        cleaned_instruction = instruction_element.text.strip()
+        instructions.append(cleaned_instruction)
+
+    return Exercise(name=title, primary_muscle=primary_muscle, image_urls=image_srcs, instructions=instructions)
 
 
 def main():
@@ -46,6 +55,5 @@ def main():
         exercises.append(exercise)
 
     # FIXME: Save data in a csv file for importing into Notion
-
 
 main()
--- august_2023/exercise.py
+++ august_2023/exercise.py
@@ -2,7 +2,8 @@ from typing import List
 
 
 class Exercise:
-    def __init__(self, name: str, primary_muscle: str, image_urls: List[str]):
+    def __init__(self, name: str, primary_muscle: str, image_urls: List[str], instructions: List[str]):
         self.name = name
         self.primary_muscle = primary_muscle
         self.image_urls = image_urls
+        self.instructions = instructions
